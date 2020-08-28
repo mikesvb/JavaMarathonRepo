80b9f0e9f8ac3ebdcdf339e2de5eaf2db1db8ae3
MikeSVB <mikesvb@mail.ru>
2020-08-28 21:59:21 +0900

    Add Day4 solutions. check index

--- src/day4/Task4.java
+++ src/day4/Task4.java
@@ -14,7 +14,7 @@ public class Task4 {
         for(int i=0; i<100; i++){
            int tempSumm;
             if(i>0 && i<99){//так как у 0 нет предыдущего, а у 99 нет последующего
-                tempSumm=arrayRandom[i-1] + arrayRandom[i-1] + arrayRandom[i+1];
+                tempSumm=arrayRandom[i-1] + arrayRandom[i] + arrayRandom[i+1];
                 if(tempSumm>maxSumm){
                     maxSumm=tempSumm;
                     index = i-1;
