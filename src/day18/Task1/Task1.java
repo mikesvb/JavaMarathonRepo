d1ab55888bbc004ccf4821fca648d744cf608f19
MikeSVB <44279181+mikesvb@users.noreply.github.com>
2020-09-11 10:15:38 +0900

    Update Task 1 in day18
    Оптимизировал использование переменных

--- src/day18/Task1/Task1.java
+++ src/day18/Task1/Task1.java
@@ -1,27 +1,21 @@
 package day18.Task1;
 
 public class Task1 {
-
-
-    private static int i=0;
-    private static int summ=0;
+	
     public static void main(String[] args) {
         int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
-        int length = numbers.length;
-        System.out.println(someMethod(numbers,length));
+        System.out.println(someMethod(numbers, 0, 0));
 
     }
-    public static Integer someMethod(int[] num, int length){
-
-        if(i>=length){
-            return summ;
+    
+    public static int someMethod(int[] num, int i, int summ){
+        if(i>=num.length){
+        	return summ;
         }else{
-            summ+=num[i];
+        	summ+=num[i];
             i++;
+            return someMethod(num, i, summ);
         }
-
-        someMethod(num, length);
-
-        return summ;
     }
+    
 }
