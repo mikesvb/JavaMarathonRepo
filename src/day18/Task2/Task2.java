1c5485146001378e9528dc73da0a4752b4ca49c7
MikeSVB <44279181+mikesvb@users.noreply.github.com>
2020-09-11 10:28:35 +0900

    Task2 in day18
    Оптимизировал переменные во 2 задании 18 дня. Используются только аргументы метода

--- src/day18/Task2/Task2.java
+++ src/day18/Task2/Task2.java
@@ -1,23 +1,17 @@
 package day18.Task2;
 
 public class Task2 {
-    private static Integer count=0;
+
     public static void main(String[] args) {
-        System.out.println(count7(717371737)); // 5
+        System.out.println(count7(717371737,0)); // 5
     }
 
-    public static Integer count7(Integer num){
-
+    public static int count7(int num,int count){
         if(num%10==7){
             count++;
         }
-
         if(num<=0){return count;}
-
         num/=10;
-
-        count7(num);
-
-        return count;
+        return count7(num, count);
     }
 }
